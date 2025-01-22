4e20f610c8a02335124cd493e2f2b45365b3bcce
Masa Ljekocevic <masaljekocevic@gmail.com>
2025-01-22 17:56:29 +0100

    finished CRUD tasks and sprints

--- calendar-backend/routes/api.php
+++ calendar-backend/routes/api.php
@@ -11,20 +11,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-//Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
-//Route::post('reset-password', [AuthController::class, 'resetPassword']);
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login'])->name('login');
 
 
-
-
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
-    // Route::group(['middleware' => ['auth:developer']], function () {
-    //     Route::resource('tasks', TaskController::class);
-    // });
+    Route::resource('tasks', TaskController::class);
 
     Route::group(['middleware' => ['auth:scrum-master']], function () {
         Route::get('sprints', [SprintController::class, 'index'])->name('sprints.index');
@@ -36,12 +30,6 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::group(['middleware' => ['auth:scrum-master,product-owner']], function () {
         Route::get('sprints/{id}', [SprintController::class, 'show'])->name('sprints.show');
     });
-
-    Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
-    Route::post('tasks', [TaskController::class, 'store'])->name('tasks.store');
-    Route::get('tasks/{id}', [TaskController::class, 'show'])->name('tasks.show');
-    Route::put('tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
-    Route::delete('tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
   
     Route::post('logout', [AuthController::class, 'logout']);
 });
