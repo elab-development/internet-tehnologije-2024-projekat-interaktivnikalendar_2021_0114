23e20084ef9657878fa5bc85b277ba9d1096c2ab
stefankosticc <skostic806@gmail.com>
2025-02-19 10:53:32 +0100

    design for not found page

--- calendar-frontend/src/pages/NotFound.js
+++ calendar-frontend/src/pages/NotFound.js
@@ -1,5 +1,18 @@
+import "../styles/NotFound.css";
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="not-found-page">
+      <p id="er404">404</p>
+      <p className="nf-text">
+        Not Found
+        <button onClick={() => navigate(-1)}>Go Back</button>
+      </p>
+    </div>
+  );
 };
 
 export default NotFound;
